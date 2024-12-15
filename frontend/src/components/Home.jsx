7318758ee9711f5979d088769a12d1025b2fb30f
Kevin Tran <kevinat1@uci.edu>
2024-12-15 14:20:50 -0800

    done with home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,3 +1,4 @@
+import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Button from "react-bootstrap/Button";
@@ -6,37 +7,28 @@ import Form from "react-bootstrap/Form";
 
 const Home = () => {
   return (
-    <Card border="primary" className="text-center">
-      <Card.Img src="https://thumbs.dreamstime.com/b/cartoon-family-cooking-together-kitchen-chef-ai-cartoon-family-cooking-together-kitchen-chef-337451164.jpg"></Card.Img>
-      <Card.Header>
-        Have groceries in your refrigerator or pantry and don't know what to
-        make with them? Snap-Chef is the answer!
-      </Card.Header>
-      <Card.Body>
-        <Card.Title>Snap Chef</Card.Title>
-        <Card.Text>Enter your groceries</Card.Text>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
+    <div className="App">
+      <Card border="primary" className="text-center">
+        <Card.Img src="https://img.freepik.com/free-vector/hand-drawn-toast-cartoon-illustration_52683-127439.jpg?t=st=1734300872~exp=1734304472~hmac=48ee466df3f29f837a710266e448c880b1b9c65c00b1b2a022204e6e2ad1d87c&w=740"></Card.Img>
+        <Card.Header>Welcome to Snap Chef!</Card.Header>
+        <Card.Body>
+          <Card.Title>Snap Chef</Card.Title>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Enter your groceries here:</Form.Label>
+              <Form.Control type="email" placeholder="Grocieries" />
+              <Form.Text className="text-muted">
+                Have groceries in your refrigerator or pantry and don't know
+                what to make with them? Snap-Chef is the answer!{" "}
+              </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Generate Recipes
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
